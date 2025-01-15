2be86cfee706ce02776f83c0130517e2532f03dc
ssaquisi <ssaquisi@espol.edu.ec>
2025-01-14 23:40:07 -0500

    probando funciones int y float

---
+++ utilitarios/saludo.js
@@ -0,0 +1,26 @@
+saludar=function(){
+    let nombre=recuperarTexto("txtNombre");
+    let apellido=recuperarTexto("txtApellido")
+    let edad=recuperarInt("txtEdad");
+    let estatura=recuperarFloat("txtEstatura");
+}
+
+recuperarTexto=function(idComponente){
+    let componente;
+    let valorIngresado;
+    componente=document.getElementById(idComponente);
+    valorIngresado=componente.value;
+    return valorIngresado;
+}
+
+recuperarInt=function(idComponente){
+    let valorCaja=recuperarTexto(idComponente);
+    let valorEntero=parseInt(valorCaja);
+    return valorEntero;
+}
+
+recuperarFloat=function(idComponente){
+    let valorCaja=recuperarTexto(idComponente);
+    let valorFloat=parseFloat(valorCaja);
+    return valorFloat;
+}
